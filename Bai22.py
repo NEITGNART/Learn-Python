0c118ab8c0fd7b573fdd051e029d791aa25edd30
Pham Doan Tien 19127576 <63442323+NEITGNART@users.noreply.github.com>
2021-01-14 12:08:58 +0700

    Update Bai22.py

--- Bai22.py
+++ Bai22.py
@@ -20,6 +20,8 @@ def solve():
     # 0 0 0 1 1
 
     res = []
+
+
     def Try(i: int, j: int, s:str):
 
         if (i < 0 or i >= n or j < 0 or j >= n or grid[i][j] == 0):
@@ -34,10 +36,10 @@ def solve():
         if (j + 1 < n and grid[i][j + 1] == 1):
             Try(i, j + 1, s + "R")
 
-    Try(0, 0, '')
-    for s in res:
-        print(s)
-
+    if (grid[0][0] == 1):
+        Try(0, 0, '')
+    else:
+        print("No solution")
 
 def main():
     solve()
