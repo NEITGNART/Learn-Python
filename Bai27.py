bac5757699ee5588c94f636c65e6975594be2f28
Pham Doan Tien 19127576 <63442323+NEITGNART@users.noreply.github.com>
2021-01-20 00:47:42 +0700

    Update Bai27.py

--- Bai27.py
+++ Bai27.py
@@ -10,4 +10,23 @@ def staircase(n, m):
 
   return dp[n]
 
-stressTesting = True
+''' Trick with python. Another way python bring powerful tools '''
+
+import sys
+sys.setrecursionlimit(5000)
+from functools import lru_cache
+@lru_cache(maxsize = None)
+def staircase(n, m):
+  # base case of when there is no stair
+  if n == 0:    
+    return 1
+  ways = 0
+  # iterate over number of steps, we can take
+  for i in range(1,m+1):  
+    # if steps remaining is smaller than the jump step, skip   
+    if i <= n:  
+      # recursive call with n i units lesser where i is the number of steps taken here            
+      ways += staircase(n-i, m) 
+  return ways
+
+print(staircase(1500,6)) # this number produce a huge of solution
